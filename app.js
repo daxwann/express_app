2e636b98d23998d03d52f7f3eace1938189ff895
Dax Wann <daxwann@gmail.com>
2021-11-13 21:00:21 -0800

    add routes and views

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const shopRouter = require('./routes/shop');
+const adminRouter = require('./routes/admin');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.use('/admin', adminRouter);
+app.use(shopRouter);
+app.use("/", (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(3000);
