ecf7817ad8d11cae87b8ec4cd5b7aa328a570f2b
Dax Wann <daxwann@gmail.com>
2021-11-17 22:06:07 -0800

    create products state

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const adminRouter = require('./routes/admin');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/admin', adminRouter);
+app.use('/admin', adminRouter.router);
 app.use(shopRouter);
 app.use("/", (req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,10 @@ const express = require('express');
 const rootDir = require('../util/rootDir');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 })
 
